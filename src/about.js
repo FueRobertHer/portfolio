770b4a3247a1532408d34684e9cbe1c87d7e1e11
Fue <fher.209@gmail.com>
2020-05-01 14:49:44 -0700

    Get scroll to section working

--- src/about.js
+++ src/about.js
@@ -1,78 +1,59 @@
-import React from 'react';
+import React, {useRef, useEffect} from 'react';
 import './styles/about.css'
 
+
+
 function About() {
+  let overviewRef = useRef(null)
+  const workRef = useRef(null)
+  const personalRef = useRef(null)
+  let hobbiesRef = useRef(null)
+
+  let about = document.getElementById('mainabout')
+  useEffect(() => {
+    about = document.getElementById('mainabout')
+
+  })
+  const scrollToRef = (ref) => about.scrollTo(0, ref.current.offsetTop - 100)
+
+
   return (
-    <div className="about">  
+    <div id='mainabout' className="about">  
       <div className="about-nav">
         <ul className="about-nav-list">
-          <a href="#overview">OVERVIEW</a>
-          <a href="#work">WORK HISTORY</a>
-          <a href="#personal">PERSONAL HISTORY</a>
-          <a href="#hobbies">HOBBIES</a>
+          <li onClick={() => scrollToRef(overviewRef)}>OVERVIEW</li>
+          <li onClick={() => scrollToRef(workRef)}>WORK HISTORY</li>
+          <li onClick={() => scrollToRef(personalRef)}>PERSONAL HISTORY</li>
+          <li onClick={() => scrollToRef(hobbiesRef)}>HOBBIES</li>
         </ul>
       </div>
       
       <section className="about-body">
         <section>
-          <h2 id="overview">OVERVIEW</h2>
+          <h2 ref={overviewRef}>OVERVIEW</h2>
           <article>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
+            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
           </article>
         </section>
 
         <section>
-          <h2 id="work">WORK HISTORY</h2>
+          <h2 ref={workRef}>WORK HISTORY</h2>
           <article>
-            
+            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
           </article>
         </section>
 
         <section>
-          <h2 id="personal">PERSONAL HISTORY</h2>
+          <h2 ref={personalRef}>PERSONAL HISTORY</h2>
           <article>
-            
+            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
           </article>
         </section>
 
         <section>
-          <h2 id="hobbies">HOBBIES</h2>
+          <h2 ref={hobbiesRef}>HOBBIES</h2>
           <article>
-            
+            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
           </article>
         </section>
       </section>
