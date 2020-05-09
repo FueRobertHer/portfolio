e85b4c20503cd0fb9ddcaaf9e841a5a00b9ca9b3
Fue <fher.209@gmail.com>
2020-05-08 17:17:34 -0700

    Fix flexbox css

--- src/home.js
+++ src/home.js
@@ -13,26 +13,24 @@ function Home(props) {
 
   return (
     <div className="home">  
-      <article>
         <h2 className="tagline">Building apps to open pathways to <strong className="strong">opportunities, empowerment, and positive impacts</strong></h2>
 
-        {/* <p>
-          I'm a software engineer that likes to build cool things and have fun. Before entering the software world, I worked in ecological restoration as a Technician in government and as an Inventory Specialist in a Native Plant Nursery. I love to tackle difficult and interesting problems to hopefully create a positive impact on the world.
-        </p>
+      {/* <p>
+        I'm a software engineer that likes to build cool things and have fun. Before entering the software world, I worked in ecological restoration as a Technician in government and as an Inventory Specialist in a Native Plant Nursery. I love to tackle difficult and interesting problems to hopefully create a positive impact on the world.
+      </p>
 
-        <p>
-          Read my story <a className="about-link" href="#" onClick={goToAbout}>here</a>.
-        </p> */}
+      <p>
+        Read my story <a className="about-link" href="#" onClick={goToAbout}>here</a>.
+      </p> */}
 
-        <div className="sky">
-          <Cloud1 classes="cloud cloud-1" />
-          <Cloud2 classes="cloud cloud-2" />
-          <Sun classes="sun" />
-          <Cloud1 classes="cloud cloud-3" />
-          <Cloud2 classes="cloud cloud-4" />
-          <Cloud1 classes="cloud cloud-5" />
-        </div>
-      </article>
+      <div className="sky">
+        <Cloud1 classes="cloud cloud-1" />
+        <Cloud2 classes="cloud cloud-2" />
+        <Sun classes="sun" />
+        <Cloud1 classes="cloud cloud-3" />
+        <Cloud2 classes="cloud cloud-4" />
+        <Cloud1 classes="cloud cloud-5" />
+      </div>
     </div>
   )
 };
