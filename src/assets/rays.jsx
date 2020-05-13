8d574e10407cd148b383b29b14b3f2b2b9ffade0
Fue <fher.209@gmail.com>
2020-05-13 11:17:13 -0700

    Add more styling

--- src/assets/rays.jsx
+++ src/assets/rays.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Rays({ classes }) {
   return (
     <svg className={classes} height="300" width="300" viewBox="0 0 300 300">
-      <g fill-opacity=".1">
+      <g fill-opacity=".05">
         <circle cx="150" cy="150" r="50" />
         <circle cx="150" cy="150" r="70" />
         <circle cx="150" cy="150" r="90" />
