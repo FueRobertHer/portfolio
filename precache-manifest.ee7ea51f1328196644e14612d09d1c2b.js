f1354eb40f85520832afdbde88f7291da3d82bea
Fue <fher.209@gmail.com>
2020-05-14 19:07:37 -0700

    Updates

---
+++ precache-manifest.ee7ea51f1328196644e14612d09d1c2b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d3b6a231f79a329ebe91153e6ca8e2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e0f91de4b933e03a845",
+    "url": "/static/css/main.6e068a93.chunk.css"
+  },
+  {
+    "revision": "18bd686a1e44e217511c",
+    "url": "/static/js/2.c7d29f89.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c7d29f89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e0f91de4b933e03a845",
+    "url": "/static/js/main.caf16e44.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  },
+  {
+    "revision": "46bbdbdaa5ad17cfeec772fc48d06517",
+    "url": "/static/media/family_photo.46bbdbda.jpg"
+  },
+  {
+    "revision": "fecfc014869e919fde4061e048a8435d",
+    "url": "/static/media/jumpykart.fecfc014.png"
+  },
+  {
+    "revision": "879b31c79e4c6d249b524a8d458f3cb9",
+    "url": "/static/media/nspect.879b31c7.png"
+  },
+  {
+    "revision": "46054cb2077b0cf9a758b0807df99d21",
+    "url": "/static/media/pic.46054cb2.jpg"
+  },
+  {
+    "revision": "4aad81996183f1943db4675e0b3d5ace",
+    "url": "/static/media/planting_day_GGNPC.4aad8199.jpeg"
+  },
+  {
+    "revision": "a370467ccdc83ffbd8acc58ce87edc3c",
+    "url": "/static/media/salmonrun.a370467c.png"
+  },
+  {
+    "revision": "2fb502cc2c939dcbbbceccb6ffcb8862",
+    "url": "/static/media/work-in-progress.2fb502cc.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8cf24b60d42b0522642546f1607cb10e.js"
+  "/precache-manifest.ee7ea51f1328196644e14612d09d1c2b.js"
 );
 
 self.addEventListener('message', (event) => {
